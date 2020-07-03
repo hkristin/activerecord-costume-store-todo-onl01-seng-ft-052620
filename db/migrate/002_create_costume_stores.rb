aafdc7dac60979569ed9b08a105337f7ffabecbd
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-03 02:15:34 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,9 +3,12 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
+      t.datetime :opening_time
+      t.datetime :closing_time
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees_count
+      t.integer :num_of_employees
+      t.boolean :still_in_buisness
       t.timestamps
     end
   end
