2b05a9eabc4b6d7a964fd5372b2fea2ef536eff0
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-03 01:50:33 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,9 +14,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.string :image_url
-      t.integer :size
+      t.string :size
     end
   end
   
