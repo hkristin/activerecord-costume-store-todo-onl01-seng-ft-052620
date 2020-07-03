18e9546bc599ee2d72ac6321d79c07796825f33b
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-03 02:08:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.integer :employees_count
+      t.timestamps
+    end
+  end
+  
+end
